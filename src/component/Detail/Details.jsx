92bb3f2158a595e853be706e1141794187bfb50f
sawankumar-pal-au9 <manojpal211999@gmail.com>
2024-08-13 22:16:16 +0530

    add the details

--- src/component/Detail/Details.jsx
+++ src/component/Detail/Details.jsx
@@ -1,28 +1,20 @@
 import React from 'react'
-import "../../styles/Details.css"
 
+const DetailItem = ({ title, content }) => (
+    <div className="relative flex flex-col items-start min-w-[150px] pl-[50px] mb-4">
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 w-10 h-10 border border-[#e9e7e7] rounded-lg"></div>
+        <h2 className="text-[#333] text-sm m-0">{title}</h2>
+        <p className="text-[#555] text-xs">{content}</p>
+    </div>
+)
 
-const Details = () => {
-    return (
-        <div className="details-container">
-            <div className="detail-item">
-                <h2>Duration</h2>
-                <p>3 days</p>
-            </div>
-            <div className="detail-item">
-                <h2>Group Size</h2>
-                <p>5 people</p>
-            </div>
-            <div className="detail-item">
-                <h2>Ages</h2>
-                <p>18-99 yrs</p>
-            </div>
-            <div className="detail-item">
-                <h2>Languages</h2>
-                <p>English, Japanese</p>
-            </div>
-        </div>
-    )
-}
+const Details = () => (
+    <div className="flex flex-wrap justify-start py-4">
+        <DetailItem title="Duration" content="3 days" />
+        <DetailItem title="Group Size" content="5 people" />
+        <DetailItem title="Ages" content="18-99 yrs" />
+        <DetailItem title="Languages" content="English, Japanese" />
+    </div>
+)
 
 export default Details
